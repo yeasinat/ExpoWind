ee4a8cbde9bcd98f88d9871d35f115d886974483
Y3ASIN <yeasinarafat.wa@gmail.com>
2025-05-18 14:32:27 +0600

    fix: nativewind path.

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
