2d2320c584dd1a0c67e67e0e93f03d927ce0ebfc
Y3ASIN <yeasinarafat.wa@gmail.com>
2025-05-18 14:33:26 +0600

    test: working

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-     
-    >
-      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
